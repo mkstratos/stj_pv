47da7847d054e28206bfb46bfc575066b0cc2e07
Michael Kelleher <kelleher.mike@gmail.com>
2017-04-26 12:35:36 +0100

    Adds doc generation options

--- STJ_PV/refac/doc/conf.py
+++ STJ_PV/refac/doc/conf.py
@@ -41,9 +41,10 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
+    'sphinx.ext.autosummary',
     'numpydoc'
 ]
-
+numpydoc_show_class_members = False
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
