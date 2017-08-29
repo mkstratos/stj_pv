0c16b2a4bc922ad2b9111f2c5c7da75f9f4338bd
Michael Kelleher <kelleher.mike@gmail.com>
2017-08-29 11:16:11 +0100

    Updates configuration files for inclusion of strf

--- STJ_PV/run_stj.py
+++ STJ_PV/run_stj.py
@@ -277,7 +277,8 @@ def check_data_config(cfg_file):
 def main():
     """Run the STJ Metric given a configuration file."""
     # Generate an STJProperties, allows easy access to these properties across methods.
-    jf_run = JetFindRun('./conf/stj_config_erai_monthly_gv.yml')
+    #jf_run = JetFindRun('./conf/stj_config_erai_monthly_gv.yml')
+    jf_run = JetFindRun('./conf/stj_config_ncep.yml')
     #jf_run.run(1979, 2016)
     jf_run._get_data(1979)
     jf_run.log.info('JET FINDING COMPLETE')
