cfbf5da774d8315d01ea63cd1472c84b20fec24f
Michael Kelleher <kelleher.mike@gmail.com>
2018-06-14 12:11:23 +0100

    Adds loop to summarise multiple runs, shortens lns

--- STJ_PV/summary_plots.py
+++ STJ_PV/summary_plots.py
@@ -5,14 +5,14 @@ import numpy as np
 import xarray as xr
 import matplotlib.pyplot as plt
 
+
 __author__ = 'Michael Kelleher'
 
 
-def main(props=None):
+def main(run_name=None, props=None):
     """Load jet run output, make a plot or two."""
     if props is None:
-        props = {'data': 'NCEP_NCAR_MONTHLY_STJPV',
-                 'pv': 2.0,
+        props = {'pv': 2.0,
                  'fit': 6,
                  'y0': 10.0,
                  'yN': 65.0,
@@ -20,10 +20,15 @@ def main(props=None):
                  'date_s': '1979-01-01',
                  'date_e': '2016-12-31'}
 
+    if run_name is None:
+        props['data'] = 'ERAI_MONTHLY_THETA_STJPV'
+    else:
+        props['data'] = run_name
+
     plt.rc('font', family='sans-serif', size=10)
 
-    in_file = ('{data}_pv{pv:.1f}_fit{fit}_y0{y0:03.1f}_yN{yN:.1f}_z{zonal_reduce}'
-               '_{date_s}_{date_e}'.format(**props))
+    in_file = ('{data}_pv{pv:.1f}_fit{fit}_y0{y0:03.1f}_yN{yN:.1f}_'
+               'z{zonal_reduce}_{date_s}_{date_e}'.format(**props))
 
     data = xr.open_dataset(f'{in_file}.nc')
 
@@ -38,10 +43,18 @@ def main(props=None):
             plt.subplot2grid((2, 2), (1, 0), colspan=2)]
 
     for hidx, hem in enumerate(['sh', 'nh']):
-        sct = axes[hidx].scatter(data[f'lat_{hem}'].values, data[f'theta_{hem}'].values,
+        sct = axes[hidx].scatter(data[f'lat_{hem}'].values,
+                                 data[f'theta_{hem}'].values,
                                  s=data[f'intens_{hem}'].values * 4,
-                                 c=data[f'intens_{hem}'].values, cmap='inferno',
-                                 vmin=0.0, vmax=45.0, alpha=0.4)
+                                 c=data[f'intens_{hem}'].values,
+                                 cmap='inferno', vmin=0., vmax=45., alpha=0.4)
+
+        # Hexbins? Maybe...
+        # sct = axes[hidx].hexbin(data[f'lat_{hem}'].values,
+        #                         data[f'theta_{hem}'].values,
+        #                         gridsize=20, cmap='Blues', mincnt=1,
+        #                         edgecolors='k', linewidths=0.1)
+
         axes[hidx].set_ylabel('Theta Position [K]')
         if hem == 'nh':
             cax = fig.add_axes([0.48, 0.51, 0.02, 0.4])
@@ -55,28 +68,27 @@ def main(props=None):
 
         axes[hidx].set_xlabel('Latitude Position [deg]')
         axes[hidx].set_title(hem_info[hem]['label'])
-        axes[hidx].set_ylim([340, 370])
+        axes[hidx].set_ylim([320, 370])
         axes[hidx].set_xlim(hem_info[hem]['lat_r'])
 
     ln_nh = axes[2].plot(month_mean.month, month_mean['lat_nh'].values,
                          'C0o-', label='NH')
 
     axes[2].fill_between(month_mean.month,
-                         month_mean['lat_nh'].values + month_std['lat_nh'].values,
-                         month_mean['lat_nh'].values - month_std['lat_nh'].values,
+                         month_mean['lat_nh'] + month_std['lat_nh'],
+                         month_mean['lat_nh'] - month_std['lat_nh'],
                          color='C0', alpha=0.6)
 
     axes[2].set_ylim(hem_info['nh']['lat_r'])
     axes[2].set_ylabel('NH Jet Latitude')
 
     axis_sh = axes[2].twinx()
-    axis_sh.invert_yaxis()
 
-    ln_sh = axis_sh.plot(month_mean.month, month_mean['lat_sh'].values,
-                         'C1o-', label='SH')
+    ln_sh = axis_sh.plot(month_mean.month, month_mean['lat_sh'], 'C1o-',
+                         label='SH')
     axis_sh.fill_between(month_mean.month,
-                         month_mean['lat_sh'].values + month_std['lat_sh'].values,
-                         month_mean['lat_sh'].values - month_std['lat_sh'].values,
+                         month_mean['lat_sh'] + month_std['lat_sh'],
+                         month_mean['lat_sh'] - month_std['lat_sh'],
                          color='C1', alpha=0.6)
     axis_sh.set_ylim(hem_info['sh']['lat_r'])
 
@@ -92,6 +104,7 @@ def main(props=None):
     labs = [l.get_label() for l in lns]
     axes[2].legend(lns, labs, loc=0)
     axes[2].grid(b=True, ls='--')
+    axis_sh.invert_yaxis()
 
     data_name = ' '.join(props['data'].split('_'))
     plt.suptitle('{}: {pv} PVU, lat [{y0} - {yN}], '
@@ -99,8 +112,9 @@ def main(props=None):
     fig.subplots_adjust(left=0.06, bottom=0.04, right=0.93, top=0.91,
                         wspace=0.20, hspace=0.20)
     plt.savefig(f'{in_file}.png')
-    plt.show()
 
 
 if __name__ == '__main__':
-    main()
+    for RNAME in ['NCEP_NCAR_MONTHLY_STJPV', 'NCEP_NCAR_DAILY_STJPV',
+                  'ERAI_MONTHLY_THETA_STJPV', 'ERAI_DAILY_THETA_STJPV']:
+        main(run_name=RNAME)
